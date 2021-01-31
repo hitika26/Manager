ca31ab2623eaee72538e1016ed5368a8e6e79dbd
Ashish Kumar Sharma <ashish.k.sharma@intimetec.com>
2021-01-31 21:53:57 +0530

    Ashish Commit

---
+++ countOccurence.ps1
@@ -0,0 +1,23 @@
+﻿param(
+    [string]$inputString,
+    [string]$inputChar
+)
+
+function Get-CountOccurence{
+   try{
+       if($inputChar.Length -gt 1){
+            Write-Output "You enter string in second argument instead of character"
+            return
+        }
+        $count=0
+        for ($iterator=0; $iterator -lt $inputString.Length; $iterator++) { 
+            if($inputString[$iterator]-eq $inputChar){
+              $count++
+            }
+        }
+        Write-Output "The count of your character is "$count
+    }catch{
+        Write-Output "format to input string is String chracter you might enter wrong format"   
+    }
+}
+Get-CountOccurence
